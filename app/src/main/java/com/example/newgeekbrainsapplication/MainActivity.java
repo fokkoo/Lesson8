559b51c91543c1921df3107d53c547c591f0e7b3
KazanskiiPN <fokkoo@yandex.ru>
2021-08-19 17:36:50 +0300

    try firebase

--- app/src/main/java/com/example/newgeekbrainsapplication/MainActivity.java
+++ app/src/main/java/com/example/newgeekbrainsapplication/MainActivity.java
@@ -16,7 +16,7 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    // 49 57
+    // 2 19 49
     private itemAdapter adapter;
     private CardSource cardSource;
     private RecyclerView recyclerView;
