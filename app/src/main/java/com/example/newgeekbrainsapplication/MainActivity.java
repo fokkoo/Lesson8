977cac00ab2b7a5d83595fe18f42b70092d891bb
KazanskiiPN <fokkoo@yandex.ru>
2021-08-25 17:32:28 +0300

    try firebase check bug

--- app/src/main/java/com/example/newgeekbrainsapplication/MainActivity.java
+++ app/src/main/java/com/example/newgeekbrainsapplication/MainActivity.java
@@ -15,8 +15,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
-    // 2 19 49
+    // 2 25 19
     private itemAdapter adapter;
     private CardSource cardSource;
     private RecyclerView recyclerView;
@@ -89,7 +91,12 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_add:
-                cardSource.addCardData(new CardData("new title", "new description", R.drawable.nature1, false));
+
+                CardData cardData = new CardData("new title", "new description", R.drawable.nature1, false);
+                cardData.setId(UUID.randomUUID().toString()); // генерирование уникального ключа для карточки
+
+
+                cardSource.addCardData(cardData);
                 adapter.notifyItemChanged(cardSource.size() - 1); // уведомление адаптера о обновлении списка в конкретном месте
                 recyclerView.scrollToPosition(cardSource.size() - 1);
                 return true;
@@ -117,7 +124,11 @@ public class MainActivity extends AppCompatActivity {
 
                 return true;
             case R.id.action_update:
-                cardSource.updateCardData(currentPosition, new CardData("new Title", "Description", R.drawable.nature1, false));
+
+                CardData cardData = new CardData("new Title", "Description", R.drawable.nature1, false);
+                cardData.setId(cardSource.getCardData(currentPosition).getId());
+
+                cardSource.updateCardData(currentPosition, cardData);
                 adapter.notifyItemChanged(currentPosition); // уведомление адаптера о обновлении списка
                 return true;
         }
