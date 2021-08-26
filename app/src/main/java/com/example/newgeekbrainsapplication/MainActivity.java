2e25a5a0f5afb3b63342b5f97253a08dcf088f44
KazanskiiPN <fokkoo@yandex.ru>
2021-08-26 14:41:29 +0300

    lesson 10  fire base

--- app/src/main/java/com/example/newgeekbrainsapplication/MainActivity.java
+++ app/src/main/java/com/example/newgeekbrainsapplication/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private int currentPosition = -1;
 
-    //
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
