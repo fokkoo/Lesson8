b8f1ae049fa07c0e91e389b29e6fb8130101fc8a
KazanskiiPN <fokkoo@yandex.ru>
2021-08-27 15:37:56 +0300

    lesson 10  fire base 2:05

--- app/src/main/java/com/example/newgeekbrainsapplication/CardsSourceFirebaseImpl.java
+++ app/src/main/java/com/example/newgeekbrainsapplication/CardsSourceFirebaseImpl.java
@@ -1,7 +1,13 @@
 package com.example.newgeekbrainsapplication;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,37 +26,54 @@ public class CardsSourceFirebaseImpl implements CardSource {
 
     @Override
     public CardSource init(CardSourceResponse response) {
-        return null;
+
+        collection.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            cards = new ArrayList<>();
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                      
+
+                            }
+                        }
+                    }
+                });
+
+        return this;
     }
 
     @Override
     public CardData getCardData(int position) {
-        return null;
+        return cards.get(position);
     }
 
     @Override
     public void deliteCardData(int position) {
-
+        collection.document(cards.get(position).getId()).delete();
+        cards.remove(position);
     }
 
     @Override
     public void updateCardData(int position, CardData cardData) {
 
-        cards.set(position, cardData);
+
         collection.document(cardData.getId()).set(cardData);
+        cards.set(position, cardData);
     }
 
     @Override
     public void addCardData(CardData cardData) {
         collection.document(cardData.getId()).set(cardData);
-
+        cards.add(cardData);
     }
 
     @Override
     public void clearCardData() {
-       for (CardData cardData:cards){
-           collection.document(cardData.getId()).delete(); // sinhronise wiht server
-       }
+        for (CardData cardData : cards) {
+            collection.document(cardData.getId()).delete(); // sinhronise wiht server
+        }
 
         cards.clear();
     }
