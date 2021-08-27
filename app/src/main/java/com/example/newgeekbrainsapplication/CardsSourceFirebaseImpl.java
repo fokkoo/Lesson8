4adf910ddf8eb0d8955d203fceef81ef3a5cea31
KazanskiiPN <fokkoo@yandex.ru>
2021-08-27 15:53:56 +0300

    lesson 10  fire base 2:17

--- app/src/main/java/com/example/newgeekbrainsapplication/CardsSourceFirebaseImpl.java
+++ app/src/main/java/com/example/newgeekbrainsapplication/CardsSourceFirebaseImpl.java
@@ -1,8 +1,11 @@
 package com.example.newgeekbrainsapplication;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -28,17 +31,25 @@ public class CardsSourceFirebaseImpl implements CardSource {
     public CardSource init(CardSourceResponse response) {
 
         collection.get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            cards = new ArrayList<>();
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                      
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        cards = new ArrayList<>(); // если запрос успешный
+                        for (QueryDocumentSnapshot document : task.getResult()) {
 
-                            }
+                            CardData data = document.toObject(CardData.class); // получение класса который записан в <>
+                            data.setId(document.getId()); // получение айди обратно для сохранения
+
+                            cards.add(data);// сохранение в массив карточек
                         }
+
+                        Log.d(TAG, "success");
+
+                    } else {
+                        Log.e(TAG, "failed");
                     }
+                })
+                .addOnFailureListener(e -> {
+                    Log.e(TAG, "failed",e);
                 });
 
         return this;
